6a8d2523e4a158492947d1f89bb9a5b7c1b2bfa4
adam-knight1 <adam.knight@mykenzie.snhu.edu>
2023-09-21 11:09:21 -0400

    Working on user model

--- Application/src/main/java/com/kenzie/appserver/service/model/User.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/User.java
@@ -1,7 +1,64 @@
 package com.kenzie.appserver.service.model;
 
+import java.util.UUID;
+
 public class User {
+    //just testing git branching - adam
+    UUID userID;
     String username;
     String password;
+    String email;
 
+    public User() {
+        this.userID = UUID.randomUUID();
+    }
+
+    public User(String username, String password, String email) {
+        this.userID = UUID.randomUUID();
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
