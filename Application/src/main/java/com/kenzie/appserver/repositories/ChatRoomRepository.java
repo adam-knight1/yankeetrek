a20fe47fb3e11f4ea3b7e5a6c5385d2cca216430
lyonsminton <Alexis.lyons@mykenzie.snhu.edu>
2023-09-24 20:43:09 -0400

    worked on ChatRoomRepository,UML,ChatRoomRecors, and added ChatRoomService

--- Application/src/main/java/com/kenzie/appserver/repositories/ChatRoomRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/ChatRoomRepository.java
@@ -2,7 +2,10 @@ package com.kenzie.appserver.repositories;
 import com.kenzie.appserver.repositories.model.ChatRoomRecord;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
 @EnableScan
 public interface ChatRoomRepository extends CrudRepository<ChatRoomRecord, String> {
-    //added by adam
+   // List<ChatRoomRecord> findByUserId(String userId);
 }
--- Application/src/main/java/com/kenzie/appserver/repositories/model/ChatRoomRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/ChatRoomRecord.java
@@ -1,4 +1,60 @@
 package com.kenzie.appserver.repositories.model;
 
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.Objects;
+
+@DynamoDBTable(tableName = "ChatRoom")
 public class ChatRoomRecord {
+
+    @DynamoDBHashKey
+    private String userId;
+
+    @DynamoDBAttribute
+    private String message;
+
+    @DynamoDBRangeKey
+    private Long timeStamp;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getDate() {
+        return timeStamp;
+    }
+
+    public void setDate(Long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChatRoomRecord that = (ChatRoomRecord) o;
+        return Objects.equals(userId, that.userId) && Objects.equals(message, that.message)
+                && Objects.equals(timeStamp, that.timeStamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, message, timeStamp);
+    }
 }
