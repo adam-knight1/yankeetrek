5d983b12d944046a7af0f315c3b95ae8bfb9f404
lyonsminton <Alexis.lyons@mykenzie.snhu.edu>
2023-09-25 20:47:38 -0400

    added into ChatRoom under the model package

--- Application/src/main/java/com/kenzie/appserver/repositories/model/ChatRoomRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/ChatRoomRecord.java
@@ -5,7 +5,9 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import com.kenzie.appserver.service.model.Comment;
 
+import java.util.List;
 import java.util.Objects;
 
 @DynamoDBTable(tableName = "ChatRoom")
@@ -17,8 +19,21 @@ public class ChatRoomRecord {
     @DynamoDBAttribute
     private String message;
 
+    @DynamoDBAttribute
+    private String topicName;
+
+    @DynamoDBAttribute
+    private List<Comment> commentList;
+
+    @DynamoDBAttribute
+    private String chatRoomId;
+
     @DynamoDBRangeKey
     private Long timeStamp;
+
+    public String getTopicName() {
+        return topicName;
+    }
 
     public String getUserId() {
         return userId;
@@ -36,11 +51,31 @@ public class ChatRoomRecord {
         this.message = message;
     }
 
-    public Long getDate() {
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    public String getChatRoomId() {
+        return chatRoomId;
+    }
+
+    public void setChatRoomId(String chatRoomId) {
+        this.chatRoomId = chatRoomId;
+    }
+
+    public Long getTimeStamp() {
         return timeStamp;
     }
 
-    public void setDate(Long timeStamp) {
+    public void setTimeStamp(Long timeStamp) {
         this.timeStamp = timeStamp;
     }
 
@@ -49,12 +84,11 @@ public class ChatRoomRecord {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ChatRoomRecord that = (ChatRoomRecord) o;
-        return Objects.equals(userId, that.userId) && Objects.equals(message, that.message)
-                && Objects.equals(timeStamp, that.timeStamp);
+        return Objects.equals(userId, that.userId) && Objects.equals(message, that.message) && Objects.equals(topicName, that.topicName) && Objects.equals(commentList, that.commentList) && Objects.equals(chatRoomId, that.chatRoomId) && Objects.equals(timeStamp, that.timeStamp);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userId, message, timeStamp);
+        return Objects.hash(userId, message, topicName, commentList, chatRoomId, timeStamp);
     }
 }
