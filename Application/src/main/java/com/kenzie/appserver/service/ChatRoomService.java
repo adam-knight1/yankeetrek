ef7ad45c4096b5885bbafe0c845e1281747773c8
lyonsminton <Alexis.lyons@mykenzie.snhu.edu>
2023-10-11 19:10:45 -0400

    conflict

--- Application/src/main/java/com/kenzie/appserver/service/ChatRoomService.java
+++ Application/src/main/java/com/kenzie/appserver/service/ChatRoomService.java
@@ -21,9 +21,28 @@ import java.util.List;
         this.commentService = commentService;
     }
 
-    //Do you all think "commentMessage" should be listed as Comment or String? -ALEXIS
+    //Do you all think "sentComment" should be listed as Comment or String? -ALEXIS
     //Still working on this constructor
+<<<<<<< HEAD
 
+=======
+    public ChatRoom sendComment(Comment sentComment, String ownerId ){
+        Comment comment = commentService.addNewComment(sentComment);
+        if (commentService.addNewComment(sentComment)){//.getMessageSent should be here from the comment class. i need something to close out of the message was sent or not
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "bad request");
+        }
+
+        ChatRoomRecord chatRoomRecord = new ChatRoomRecord();
+        chatRoomRecord.setOwnerId(ownerId);
+        chatRoomRecord.setTopicName(chatRoomRecord.getTopicName());
+        //do i need another .setcomment? example from unit 4 project
+        chatRoomRecord.setChatRoomId(chatRoomRecord.getChatRoomId());
+        chatRoomRecord.setTimeStamp(chatRoomRecord.getTimeStamp());
+        chatRoomRepository.save(chatRoomRecord);
+
+        return new ChatRoom(chatRoomRecord.getOwnerId(), chatRoomRecord.getChatRoomId(), chatRoomRecord.getTimeStamp());
+    }
+>>>>>>> 6ce831d (switch)
 
     public List<ChatRoom> findAll(){
         List<ChatRoom> chatRooms = new ArrayList<>();
