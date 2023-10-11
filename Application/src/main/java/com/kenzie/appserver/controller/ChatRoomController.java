ebfeb2ed3b197f17547862705282342b843c92b3
brandistaats <brandi.staats@mykenzie.snhu.edu>
2023-10-11 16:44:51 -0400

    commented out ChatRoomController methods to allow local deployment

--- Application/src/main/java/com/kenzie/appserver/controller/ChatRoomController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/ChatRoomController.java
@@ -25,20 +25,20 @@ public class ChatRoomController {
         this.commentService = commentService;
     }
     //@postmapping
-    @PostMapping
-    public ResponseEntity<ChatRoomResponse> chatRoom(
-            @RequestBody ChatRoomCreateRequest chatRoomCreateRequest){
-        // call the chatroomservice.sendcomment method with the commentmessafe and owneris from the reqquest.
-        ChatRoom chatRooms = chatRoomService.sendComment(chatRoomCreateRequest.getcommentMessage(), chatRoomCreateRequest
-                .getownerId());
-                //then convert the resulting chatrooms into a chatroomresponse and return it.
-        ChatRoomResponse chatRoomResponse = new ChatRoomResponse();
-        chatRoomResponse.setChatRoomId(chatRooms.getChatRoomId());
-        chatRoomResponse.setOwnerId(chatRooms.getUserId());
-        chatRoomResponse.setTimestamp(chatRooms.getTimeStamp());
-
-        return ResponseEntity.ok(chatRoomResponse);
-    }
+//    @PostMapping
+//    public ResponseEntity<ChatRoomResponse> chatRoom(
+//            @RequestBody ChatRoomCreateRequest chatRoomCreateRequest){
+//        // call the chatroomservice.sendcomment method with the commentmessafe and owneris from the reqquest.
+//        ChatRoom chatRooms = chatRoomService.sendComment(chatRoomCreateRequest.getcommentMessage(), chatRoomCreateRequest
+//                .getownerId());
+//                //then convert the resulting chatrooms into a chatroomresponse and return it.
+//        ChatRoomResponse chatRoomResponse = new ChatRoomResponse();
+//        chatRoomResponse.setChatRoomId(chatRooms.getChatRoomId());
+//        chatRoomResponse.setOwnerId(chatRooms.getUserId());
+//        chatRoomResponse.setTimestamp(chatRooms.getTimeStamp());
+//
+//        return ResponseEntity.ok(chatRoomResponse);
+//    }
     @GetMapping("/all")
     public ResponseEntity<List<ChatRoomResponse>> getAllComments() {
         List<ChatRoom> chatRooms = chatRoomService.findAll();
@@ -49,7 +49,7 @@ public class ChatRoomController {
         ChatRoomResponse chatRoomResponse = new ChatRoomResponse();
         chatRoomResponse.setChatRoomId(chatRoom.getChatRoomId());
         chatRoomResponse.setOwnerId(chatRoom.getUserId());
-        chatRoomResponse.setTimestamp(chatRoom.getTimeStamp());
+//        chatRoomResponse.setTimestamp(chatRoom.getTimeStamp());
         return chatRoomResponse;
 
 
