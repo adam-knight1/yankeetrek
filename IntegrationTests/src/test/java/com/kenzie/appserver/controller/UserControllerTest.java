82dabc0afb4b05eb0e57ef0de1ebebfbcacfce28
brandistaats <brandi.staats@mykenzie.snhu.edu>
2023-10-12 16:36:54 -0400

    Worked on UserControllerTest

--- IntegrationTests/src/test/java/com/kenzie/appserver/controller/UserControllerTest.java
+++ IntegrationTests/src/test/java/com/kenzie/appserver/controller/UserControllerTest.java
@@ -4,13 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kenzie.appserver.IntegrationTest;
 import com.kenzie.appserver.controller.model.UserResponse;
 import com.kenzie.appserver.service.UserService;
+import com.kenzie.appserver.service.model.Example;
 import com.kenzie.appserver.service.model.User;
 import net.andreinc.mockneat.MockNeat;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.util.UUID;
 
 import static java.util.UUID.randomUUID;
 import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
@@ -31,14 +36,28 @@ public class UserControllerTest {
     private final ObjectMapper mapper = new ObjectMapper();
 
     @Test
-    public void getUser_successful() throws Exception {
+    public void getUser_Exists() throws Exception {
         User user = new User();
-        userService.createNewUser(user);
-
-        mvc.perform(get("/{userId}", user.getUserId())
-                    .accept(MediaType.APPLICATION_JSON))
+        User persistedUser = userService.createNewUser(user);
+        mvc.perform(get("/{userId}", persistedUser.getUserId())
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("userId")
-                        .value(is(user.getUserId())))
+                        .value(Matchers.is(user.getUserId())))
                 .andExpect(status().isOk());
+//                User user = new User();
+//        User persistedUser = userService.createNewUser(user);
+//
+//        mvc.perform(get("/{userId}", persistedUser.getUserId())
+//                    .accept(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getUser_WrongUserId_Fails() throws Exception {
+        String userId = "123456";
+
+        mvc.perform(get("/{userId}", userId)
+                    .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
